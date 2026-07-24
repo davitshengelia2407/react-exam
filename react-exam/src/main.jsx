e0c9593199c4409637504ef8c8ed77bbb14274dc
davitshengelia <157176424+davitshengelia2407@users.noreply.github.com>
2026-07-25 03:36:24 +0400

    Update main.jsx

--- react-exam/src/main.jsx
+++ react-exam/src/main.jsx
@@ -1,22 +1,22 @@
-<<<<<<< HEAD
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
-import "./index.css";
-import App from "./App.jsx";
+// <<<<<<< HEAD
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import { Provider } from "react-redux";
+// import { store } from "./store/store";
+// import "./index.css";
+// import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
-);
-=======
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </StrictMode>
+// );
+// =======
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/global.module.css";
 
 createRoot(document.getElementById("root")).render(<App />);
->>>>>>> 4fe9d87152a7bf1b82ff2f05596d99acc30dc045
+// >>>>>>> 4fe9d87152a7bf1b82ff2f05596d99acc30dc045
